10b6416da116b3b083270dd2482791657d8a4349
Lukas Nehrkorn <lukas.nehrkorn@cgi.com>
2024-06-19 13:08:39 +0200

    Initial commit

---
+++ src/main/java/com/example/supergame/SuperGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.supergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/supergame/SuperGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
