dba23bffe66ab2450c282e500ee2d3a74320e009
Jakob <rzeppa.jakob@gmail.com>
2024-06-20 13:17:31 +0200

    added getter and setter

--- src/main/java/com/example/supergame/Inventory.java
+++ src/main/java/com/example/supergame/Inventory.java
@@ -3,8 +3,12 @@ package com.example.supergame;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "inventories")
 public class Inventory {
     @Id
