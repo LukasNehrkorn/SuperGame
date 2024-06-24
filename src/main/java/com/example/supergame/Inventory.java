2d8c936557e8fa2de847385035e6c04ec8de4b9a
Jakob <rzeppa.jakob@gmail.com>
2024-06-24 14:07:46 +0200

    delete @Id

--- src/main/java/com/example/supergame/Inventory.java
+++ src/main/java/com/example/supergame/Inventory.java
@@ -1,6 +1,5 @@
 package com.example.supergame;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +10,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Inventory {
-    @Id
-    private int id;
-
     private int playerId;
 
     // private Item items;
