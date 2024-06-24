01285582eee4136808a1b586b237a4089969072b
Jakob <rzeppa.jakob@gmail.com>
2024-06-24 14:44:47 +0200

    Added Shop

---
+++ src/main/java/com/example/supergame/Shop.java
@@ -0,0 +1,14 @@
+package com.example.supergame;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Shop {
+    // private PrimaryWeapon primaryWeapon;
+    // private SecondaryWeapon secondaryWeapon;
+    // private MeleeWeapon meleeWeapon;
+}
