e002c279783adc4f890deb9512072e6debba6511
niklas.zimmermann <niklas.zimmermann@cgi.com>
2024-06-25 10:23:14 +0200

    added Shop

---
+++ src/main/java/com/example/supergame/shop/Shop.java
@@ -0,0 +1,11 @@
+package com.example.supergame.shop;
+
+import lombok.*;
+
+@Data
+@Builder
+public class Shop {
+    // private PrimaryWeapon primaryWeapon;
+    // private SecondaryWeapon secondaryWeapon;
+    // private MeleeWeapon meleeWeapon;
+}
